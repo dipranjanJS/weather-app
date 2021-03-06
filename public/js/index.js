4f01c0f6cbf7918564716a5f5c5a1c04278ecced
dipranjanJS <dipranjan16oct@gmail.com>
2021-03-07 02:00:06 +0530

    handle error

--- public/js/index.js
+++ public/js/index.js
@@ -14,19 +14,33 @@ let weather_details = document.querySelector('.weather_details');
 // Initially loading block should be visible
 loading.textContent = '';
 weather_details.textContent = '';
+
 // Function to fetch weather details
 async function getWeather() {
     let city = searchValue.value;
     // API url
     const url = `https://cors-anywhere.herokuapp.com/http://api.weatherstack.com/current?access_key=e5f4d4e154172b6c71c35ab398454479&query=${city}`;
     loading.textContent = 'Loading...';
+    weather_details.textContent = 'Weather details';
+
+    // Setting everything to empty string
+    temperature.textContent = '';
+    feels_like.textContent = '';
+    obs_time.textContent = '';
+    pressure.textContent = '';
+    uv_index.textContent = '';
+    wind_speed.textContent = '';
+    precip.textContent = '';
 
     // Fetching data from the api url
     const response = await fetch(url);
+
     // Parsing data to json
     const data = await response.json();
+
     // Storing the current data
     const currentData = data.current;
+
     // Removing loading div
     if(currentData) {
         loading.textContent = '';
@@ -38,6 +52,9 @@ async function getWeather() {
         uv_index.textContent = 'UV index- ' + currentData.uv_index;
         wind_speed.textContent = 'Current wind speed- ' + currentData.wind_speed;
         precip.textContent = 'Precipitation- ' + currentData.precip
+    }
+    else {
+        loading.textContent = 'Invalid city name';
     }
 }
 
